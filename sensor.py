35c63978bc14b311def70f7281ac64fe362bdf89
Mark Albrand <mark.albrand5@gmail.com>
2024-11-02 18:52:40 -0600

    Envío de datos
    Co-Authored-By: Jimena Hernández <77686378+kojimena@users.noreply.github.com>

--- sensor.py
+++ sensor.py
@@ -1,14 +1,16 @@
 import random
 import json
 import time
-
 from models import Data
+from kafka import KafkaProducer
 
-#from kafka import KafkaProducer
-#producer = KafkaProducer(bootstrap_servers='164.92.76.15:9092',value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-
+INTERVAL = 15
 
 class Sensor:
+    producer = KafkaProducer(
+        bootstrap_servers='164.92.76.15:9092',
+        value_serializer=lambda v: json.dumps(v).encode('utf-8')
+    )
 
     def __init__(self):
         self.data = Data(temperature=0, humidity=0, wind_direction="N")
@@ -22,11 +24,17 @@ class Sensor:
     def wind_direction(self):
         self.data.wind_direction = random.choice(["N", "NO", "O", "SO", "S", "SE", "E", "NE"])
 
-
     def record_data(self):
-        self.record_temperature()
-        self.record_humidity()
-        self.wind_direction()
+        while True:
+            self.record_temperature()
+            self.record_humidity()
+            self.wind_direction()
+            
+            json_data = json.dumps(self.data.__dict__)
+
+            self.producer.send(topic="21004", value=json_data)
+            print(f"Data sent: {self.data}\n")
+            time.sleep(INTERVAL)      
 
     def __str__(self):
         return f"Temperature: {self.data.temperature}°C, Humidity: {self.data.humidity}%, Wind Direction: {self.data.wind_direction}"
